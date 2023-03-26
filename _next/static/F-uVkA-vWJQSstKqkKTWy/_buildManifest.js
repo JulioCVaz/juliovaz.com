754502b7aa8b06c90f30049d938658d358d4e056
JulioCVaz <julio.oliveiravaz0@gmail.com>
2023-03-26 01:39:35 +0000

    Deploying to gh-pages from @ JulioCVaz/juliocvaz.github.io@040d0cafeabbbb90508e2894240c3eaccb9a2a76 🚀

---
+++ _next/static/F-uVkA-vWJQSstKqkKTWy/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/d06e893e83b08ac4.css","static/chunks/pages/index-cd90888d3539fbf3.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
