bc5feb6a3cf5cd0a89f822108bc8853ada035635
Juliovaz <julio.oliveiravaz0@gmail.com>
2023-03-30 22:55:09 -0300

    feat: improve next.config to dev env

--- next.config.js
+++ next.config.js
@@ -1,32 +1,26 @@
 /** @type {import('next').NextConfig} */
 
-const isPipelineRunning = process.env.GITHUB_ACTIONS || false;
-
-let basePath = "/juliocvaz.github.io";
-let assetPrefix = "/juliocvaz.github.io/";
-
-if (isPipelineRunning) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, ""); // "juliocvaz.github.io"
-  assetPrefix = `https://${repo}/`;
-  basePath = `/${repo}`;
-}
-
-const nextConfig = {
+let nextConfig = {
   reactStrictMode: true,
-  output: "export",
-  // trailingSlash: true,
-  assetPrefix,
-  basePath,
-  images: {
-    unoptimized: true,
-  },
-  /**
-   * TODO: create account in imgix and config CDN
-   * images: {
-    loader: 'imgix',
-    path: 'the "domain" of your Imigix source',
-  },
-   */
 };
+
+const pipelineIsRunning = process.env.GITHUB_ACTIONS || false;
+let buildOptions = {};
+
+if (pipelineIsRunning) {
+  const repositoryName = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+  buildOptions.assetPrefix = `https://${repositoryName}/`;
+  buildOptions.basePath = `/${repositoryName}`;
+  buildOptions.output = "export";
+  buildOptions.images = {
+    unoptimized: true,
+  };
+
+  nextConfig = {
+    ...nextConfig,
+    ...buildOptions,
+  };
+}
 
 module.exports = nextConfig;
