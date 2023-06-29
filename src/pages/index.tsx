e13bb514d1084560094f53171e22353c290a7ae7
Juliovaz <julio.oliveiravaz0@gmail.com>
2023-06-28 23:14:54 -0300

    feat: set all classNames to modules styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,8 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LogoDevIcon from "@mui/icons-material/LogoDev";
 
+import styles from "./index.module.scss";
+
 const Home = (props: any) => {
   return (
     <>
@@ -19,59 +21,157 @@ const Home = (props: any) => {
       </Head>
       <Button type="outline">outline</Button>
       <Button>content</Button>
-      <section>
-        <div>
-          <p>{props?.greeting}</p>
-          <Image
-            src="/profile.jpg"
-            alt="Next.js Logo"
-            width={300}
-            height={300}
-            priority
-          />
+      <div className={styles.main}>
+        <div className={styles["page-label"]}>
+          <div className="label">
+            <span>HOME</span>
+          </div>
+          <div className="label">
+            <span>ABOUT</span>
+          </div>
+          <div className="label">
+            <span>EXPERIENCES</span>
+          </div>
+          <div className="label">
+            <span>PROJECTS</span>
+          </div>
         </div>
-        <div>
-          <p>
-            I've been working with software engineering for over 6 years,
-            currently I'm working on the core team at{" "}
-            <strong>
-              <Link href="https://juntossomosmais.com.br" target="_blank">
-                Juntos Somos mais
-              </Link>
-            </strong>
-            . Working on projects with Nextjs.
-          </p>
+        <div className={styles["page-nav"]}>
+          <div className={styles["circle-nav"]}></div>
+          <div className={styles["circle-nav"]}></div>
+          <div className={styles["circle-nav"]}></div>
+          <div className={styles["circle-nav"]}></div>
         </div>
-      </section>
-      <aside>
-        <Link
-          href="https://www.linkedin.com/in/j%C3%BAlio-c%C3%A9sar-vaz-560ab4127"
-          target="_blank"
-        >
-          <LinkedInIcon
-            style={{
-              width: "40px",
-              height: "40px",
-            }}
-          />
-        </Link>
-        <Link href="https://github.com/JulioCVaz" target="_blank">
-          <GitHubIcon
-            style={{
-              width: "40px",
-              height: "40px",
-            }}
-          />
-        </Link>
-        <Link href="#" target="_blank">
-          <LogoDevIcon
-            style={{
-              width: "40px",
-              height: "40px",
-            }}
-          />
-        </Link>
-      </aside>
+        <div className={styles.container}>
+          <header className={styles.header}>
+            <div className={styles.social}>
+              <a href="https://github.com/JulioCVaz" target="_blank">
+                <img src="./icons/GitHub.svg" alt="github" />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/j%C3%BAlio-c%C3%A9sar-vaz-560ab4127/"
+                target="_blank"
+              >
+                <img src="./icons/LinkedIn.svg" alt="linkedin" />
+              </a>
+              <a href="https://dev.to/juliovaz" target="_blank">
+                <img src="./icons/Code.svg" alt="dev.to" />
+              </a>
+              <a href="https://twitter.com/juliovazbr" target="_blank">
+                <img src="./icons/Twitter.svg" alt="twitter" />
+              </a>
+            </div>
+            <div className={styles.menu}>
+              <a href="#">
+                <img src="./icons/Menu.svg" alt="menu" />
+              </a>
+            </div>
+          </header>
+
+          <section id={styles.home}>
+            <div className={styles.description}>
+              <h1 className={styles["description-greeting"]}>
+                <strong>Hi</strong>, I'm <strong>Julio Vaz</strong>
+              </h1>
+              <p className={styles["description-carrer"]}>
+                I've been working with software engineering for over 6 years,
+                currently I'm working on the core team at Juntos Somos mais.
+                I've been working with software engineering for over 6 years,
+                currently I'm working on the core team at Juntos Somos mais.
+                I've been working with software engineering for over 6 years,
+                currently I'm working on the core team at Juntos Somos mais.
+              </p>
+              <p className={styles["description-technologies"]}>
+                Working on projects with
+                <span className={styles["description-technology"]}>nextjs</span>
+              </p>
+              <div className={styles["home-links"]}>
+                <a
+                  className="button button-outline"
+                  href="https://drive.google.com/file/d/18VNj8VmHDra18t2XIfd9N1-4IkgSG33P/view?usp=sharing"
+                  target="_blank"
+                  role="button"
+                >
+                  Resume
+                </a>
+                <a className="button" href="" role="button">
+                  Blog
+                </a>
+              </div>
+            </div>
+          </section>
+          <section id={styles["about-me"]}>
+            <div className={styles["about-me-avatar"]}>
+              <img src="./public/profile.jpg" alt="avatar-profile" />
+            </div>
+            <div className={styles["about-me-description"]}>
+              <p>
+                I've been working with software engineering for over 6 years,
+                currently I'm working on the core team at Juntos Somos mais.
+                I've been working with software engineering for over 6 years,
+                currently I'm working on the core team at Juntos Somos mais.
+                I've been working with software engineering for over 6 years,
+                currently I'm working on the core team at Juntos Somos mais.
+              </p>
+              <p>
+                Working on projects with <span>nextjs</span>
+              </p>
+            </div>
+          </section>
+          <section id={styles.experiences}>
+            <div className={styles["experiences-container"]}>
+              <div className={styles["experiences-timeline"]}>
+                <div className={styles.experiences}>
+                  <div className={styles["experience-title"]}>
+                    <h3>Juntos Somos Mais</h3>
+                    <div className={styles["experience-subtitle"]}>
+                      <span>Software Engineer - Core Team</span>
+                      <span className={styles["current-experience"]}>
+                        2021 - Current
+                      </span>
+                    </div>
+                  </div>
+                  <p className={styles["experience-description"]}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+                    laoreet metus vel sem eleifend iaculis. Quisque lacinia arcu
+                    ac sem bibendum, non blandit est blandit.
+                  </p>
+                </div>
+                <div className={styles.experiences}>
+                  <div className={styles["experience-title"]}>
+                    <h3>Looqbox</h3>
+                    <div className={styles["experience-subtitle"]}>
+                      <span>Mobile Developer</span>
+                      <span>2020 - 2021</span>
+                    </div>
+                  </div>
+                  <p className={styles["experience-description"]}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+                    laoreet metus vel sem eleifend iaculis. Quisque lacinia arcu
+                    ac sem bibendum, non blandit est blandit.
+                  </p>
+                </div>
+                <div className={styles.experiences}>
+                  <div className={styles["experience-title"]}>
+                    <h3>Grupo KRS</h3>
+                    <div className={styles["experience-subtitle"]}>
+                      <span>Software Developer</span>
+                      <span>2017 - 2020</span>
+                    </div>
+                  </div>
+                  <p className={styles["experience-description"]}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+                    laoreet metus vel sem eleifend iaculis. Quisque lacinia arcu
+                    ac sem bibendum, non blandit est blandit.
+                  </p>
+                </div>
+              </div>
+              <div className={styles["experiences-mosaic"]}></div>
+            </div>
+          </section>
+          <section id={styles.projects}></section>
+        </div>
+      </div>
     </>
   );
 };
