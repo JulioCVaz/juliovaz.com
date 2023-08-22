010ed80b9f42f9e10ec5a49e6162fa88d20bc558
Juliovaz <julio.oliveiravaz0@gmail.com>
2023-08-21 23:03:46 -0300

    chore: enable mdx to next.config

--- next.config.js
+++ next.config.js
@@ -7,10 +7,10 @@ const withMDX = require("@next/mdx")({
     // If you use remark-gfm, you'll need to use next.config.mjs
     // as the package is ESM only
     // https://github.com/remarkjs/remark-gfm#install
-    remarkPlugins: [],
-    rehypePlugins: [],
+    // remarkPlugins: [],
+    // rehypePlugins: [],
     // If you use `MDXProvider`, uncomment the following line.
-    // providerImportSource: "@mdx-js/react",
+    providerImportSource: "@mdx-js/react",
   },
 });
 
