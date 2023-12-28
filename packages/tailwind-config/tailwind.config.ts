34e483ab2b9c720c49a8dc2ceac04d84a4c67576
Juliovaz <julio.oliveiravaz0@gmail.com>
2023-12-27 23:25:02 -0300

    chore: configure tailwind.config with theme

--- packages/tailwind-config/tailwind.config.ts
+++ packages/tailwind-config/tailwind.config.ts
@@ -1,9 +1,38 @@
 import type { Config } from "tailwindcss";
 
-// We want each package to be responsible for its own content.
 const config: Omit<Config, "content"> = {
   theme: {
     extend: {
+      colors: {
+        dark: "#0e1317",
+        primary: "#4651e5",
+        white: "#fefefe",
+        gray: "#929495",
+      },
+      fontSize: {
+        small: "14px",
+        medium: "18px",
+        large: "36px",
+      },
+      fontWeight: {
+        regular: "400px",
+        medium: "500px",
+        bold: "600px",
+      },
+      spacing: {
+        xxsmall: "4px",
+        xsmall: "8px",
+        small: "12px",
+        base: "16px",
+        medium: "20px",
+        large: "24px",
+        xlarge: "28px",
+        xxlarge: "32px",
+        xxxlarge: "40px",
+        xxxxlarge: "48px",
+        giant: "64px",
+        xgiant: "80px",
+      },
       backgroundImage: {
         "glow-conic":
           "conic-gradient(from 180deg at 50% 50%, #2a8af6 0deg, #a853ba 180deg, #e92a67 360deg)",
