a96c44ba4136608e64c94744cb715742980be884
Juliovaz <julio.oliveiravaz0@gmail.com>
2023-12-28 12:56:48 -0300

    fix: reset eslint and prettier config

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Page(): JSX.Element {
   return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-24 bg-dark">
-      <div className="z-10 items-center justify-between w-full max-w-5xl font-mono text-sm lg:flex"></div>
+    <main className="flex flex-col items-center bg-dark justify-between min-h-screen z-2 p-24">
+      <div className="z-10 items-center justify-between w-full max-w-5xl font-mono text-sm lg:flex" />
     </main>
   );
 }
