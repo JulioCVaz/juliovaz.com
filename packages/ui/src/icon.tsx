46069a75f9242631b566484ddf8a31972511cb2b
Juliovaz <julio.oliveiravaz0@gmail.com>
2024-01-03 17:06:06 -0300

    feat: create dynamic icon in ui package

---
+++ packages/ui/src/icon.tsx
@@ -0,0 +1,17 @@
+import { lazy, Suspense } from "react";
+import type { LucideProps } from "lucide-react";
+import dynamicIconImports from "lucide-react/dynamicIconImports";
+
+interface IconProps extends LucideProps {
+  name: keyof typeof dynamicIconImports;
+}
+
+export function Icon({ name, ...props }: IconProps) {
+  const LucideIcon = lazy(dynamicIconImports[name]);
+
+  return (
+    <Suspense fallback={<i />}>
+      <LucideIcon {...props} />
+    </Suspense>
+  );
+}
