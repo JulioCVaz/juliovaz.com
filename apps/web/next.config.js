f0d2152a6e8d2e6a66dd96cefc38e4e001adc625
Juliovaz <julio.oliveiravaz0@gmail.com>
2024-01-06 00:55:41 -0300

    feat: add temporary config for placehold domain

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,10 +3,16 @@ module.exports = {
   reactStrictMode: true,
   transpilePackages: ["lucide-react", "@westeros/ui"],
   images: {
+    // TODO: remove this
+    dangerouslyAllowSVG: true,
     remotePatterns: [
       {
         protocol: "https",
         hostname: "d111erjd7vhu4f.cloudfront.net",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
       },
     ],
   },
