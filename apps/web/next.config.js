9d4a227cce1f3c179c81065ffb1bd14d63f8c278
Juliovaz <julio.oliveiravaz0@gmail.com>
2024-01-10 21:53:32 -0300

    chore: remove svg config from next.config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,16 +3,10 @@ module.exports = {
   reactStrictMode: true,
   transpilePackages: ["lucide-react", "@westeros/ui"],
   images: {
-    // TODO: remove this
-    dangerouslyAllowSVG: true,
     remotePatterns: [
       {
         protocol: "https",
         hostname: "d111erjd7vhu4f.cloudfront.net",
-      },
-      {
-        protocol: "https",
-        hostname: "placehold.co",
       },
     ],
   },
