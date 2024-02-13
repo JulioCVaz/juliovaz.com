7c22e95040d1a82b906443369e026d1f568d6fee
Juliovaz <julio.oliveiravaz0@gmail.com>
2024-02-13 18:52:36 -0300

    feat: return all posts from mdx

--- apps/web/src/app/blog/[slug]/page.tsx
+++ apps/web/src/app/blog/[slug]/page.tsx
@@ -1,3 +1,43 @@
-export default function Post({ params }: { params: { slug: string } }) {
-  return <div>My Post: {params.slug}</div>;
+/* eslint-disable eslint-comments/require-description */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/require-await */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable @typescript-eslint/no-shadow */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
+import { getMDXComponent } from "next-contentlayer/hooks";
+import { format, parseISO } from "date-fns";
+import { allPosts } from "contentlayer/generated";
+
+export async function generateStaticParams() {
+  return allPosts.map((post) => ({ slug: post._raw.flattenedPath }));
+}
+
+export async function generateMetadata({ params }) {
+  const post = allPosts.find((post) => post._raw.flattenedPath === params.slug);
+  return { title: post.title };
+}
+
+export default function PostPage({
+  params,
+}: {
+  params: { slug: string };
+}): JSX.Element {
+  const post = allPosts.find((post) => post._raw.flattenedPath === params.slug);
+  const Content = getMDXComponent(post.body.code);
+
+  return (
+    <article className="mx-auto max-w-xl py-8">
+      <div className="mb-8 text-center">
+        <time className="mb-1 text-xs text-gray-600" dateTime={post.date}>
+          {format(parseISO(post.date), "LLLL d, yyyy")}
+        </time>
+        <h1>{post.title}</h1>
+      </div>
+      <Content />
+    </article>
+  );
 }
