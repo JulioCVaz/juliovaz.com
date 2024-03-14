034b79199aca073cc84db510f43d3b078606ba0e
Juliovaz <julio.oliveiravaz0@gmail.com>
2024-03-13 22:59:07 -0300

    feat: add filter to show posts by lang

--- apps/web/src/app/[lang]/blog/page.tsx
+++ apps/web/src/app/[lang]/blog/page.tsx
@@ -1,11 +1,18 @@
 import { compareDesc, format, parseISO } from "date-fns";
 import { allPosts, type Post } from "contentlayer/generated";
 import Card from "../../../components/card";
+import type { Locale } from "../../../i18n-config";
 
-export default function Blog(): JSX.Element {
-  const posts = allPosts.sort((a, b) => {
-    return compareDesc(new Date(a.date), new Date(b.date));
-  });
+export default function Blog({
+  params: { lang },
+}: {
+  params: { lang: Locale };
+}): JSX.Element {
+  const posts = allPosts
+    .sort((a, b) => {
+      return compareDesc(new Date(a.date), new Date(b.date));
+    })
+    .filter((post: Post) => post._id.includes(`/${lang}/`));
 
   return (
     <div className="grid grid-cols-1 gap-4">
--- apps/web/src/app/[lang]/page.tsx
+++ apps/web/src/app/[lang]/page.tsx
@@ -12,9 +12,11 @@ export default async function Page({
 }: {
   params: { lang: Locale };
 }): Promise<JSX.Element> {
-  const posts = allPosts.sort((a, b) => {
-    return compareDesc(new Date(a.date), new Date(b.date));
-  });
+  const posts = allPosts
+    .sort((a, b) => {
+      return compareDesc(new Date(a.date), new Date(b.date));
+    })
+    .filter((post: Post) => post._id.includes(`/${lang}/`));
 
   const dictionary = await getDictionary(lang);
 
